d3fc45617b99f981ffcb1afd9badd7654b60f57f
slate2b-admin <thomas.vaughn@gmail.com>
2022-11-27 17:06:13 -0700

    Added and configured basic UI components for login screen

--- app/src/main/java/com/cs360/inventorysystem/LoginActivity.java
+++ app/src/main/java/com/cs360/inventorysystem/LoginActivity.java
@@ -4,14 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        // Adding comment for testing purposes in Git / Sourcetree
 
     }
 }
