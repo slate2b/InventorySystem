78baf55e1bf3ecb512a9c9b23451a1eee2d29b94
slate2b <thomas.vaughn@gmail.com>
2022-12-19 15:33:41 -0700

    Cleaned up the code in SettingsActivity.java.

--- app/src/main/java/com/cs360/inventorysystem/SettingsActivity.java
+++ app/src/main/java/com/cs360/inventorysystem/SettingsActivity.java
@@ -24,49 +24,9 @@ public class SettingsActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
-
-//
-//        // Retrieve sms preferences
-//        boolean smsNotifications = sharedPreferences.getBoolean(SettingsFragment.PREFERENCE_SMS,
-//                false);
-//        // If sms preference is set to true
-//        if (smsNotifications) {
-//            // If permission is already granted
-//            if (ContextCompat.checkSelfPermission(
-//                    this, Manifest.permission.SEND_SMS) ==
-//                    PackageManager.PERMISSION_GRANTED) {
-//                // Leave preference set to true and recreate the activity
-//                getActivity().recreate();
-//            }
-//            else {
-//                // You can directly ask for the permission.
-//                // The registered ActivityResultCallback gets the result of this request.
-//                getActivity().getRequestPermissionLauncher().launch(
-//                        Manifest.permission.SEND_SMS);
-//            }
-//        }
-
         // Display the fragment as the main content
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     }
-
-//    private ActivityResultLauncher<String> requestPermissionLauncher =
-//            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-//                if (isGranted) {
-//                    // Permission is granted. Continue the action or workflow in your
-//                    // app.
-//
-//                } else {
-//                    // Explain to the user that the feature is unavailable because the
-//                    // feature requires a permission that the user has denied. At the
-//                    // same time, respect the user's decision. Don't link to system
-//                    // settings in an effort to convince the user to change their
-//                    // decision.
-//                }
-//            });
-
-
-
 }
